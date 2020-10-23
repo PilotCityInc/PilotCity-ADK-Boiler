863b4749e81f303365dc11d47f7f2f581b05b42d
Derick Lee <dericklee@pilotcity.com>
2020-10-23 01:36:16 -0700

    :lipstick: finalizing rfp

--- src/Module/components/ModuleMonitor.vue
+++ src/Module/components/ModuleMonitor.vue
@@ -14,80 +14,185 @@
       <br />
       <div class="module-default__youtube"></div>
       <br />
+      <br />
+
+      <div class="module-default__faq">
+        <v-btn dense color="blue" large rounded outlined depressed>KEY QUESTIONS & ANSWERS</v-btn>
+        <br />
+        <br />
+
+        <template>
+          <v-timeline>
+            <v-timeline-item v-for="n in 1" :key="n" color="green" fill-dot left large>
+              <template v-slot:icon>
+                <v-avatar>
+                  <img
+                    src="https://static.wixstatic.com/media/b0cb5d_7a6494c66dfb4bb6b9e2d6c71c214eca~mv2.jpg/v1/fill/w_432,h_434,al_c,lg_1,q_80/Brit_Pro.webp"
+                  />
+                </v-avatar>
+              </template>
+              <v-card outlined color="grey lighten-3" class="elevation-0">
+                <v-card-text
+                  ><div class="module-default__faq-answer-dark">
+                    Hi, my name is
+                    <span class="module-default__faq-answer-dark-highlight">Brittany Domnie</span>
+                    with
+                    <span class="module-default__faq-answer-dark-highlight"
+                      >Kaiser Permanente National Innovation</span
+                    >. Below are some answers to some key questions.
+                  </div></v-card-text
+                >
+              </v-card>
+            </v-timeline-item>
+
+            <v-timeline-item v-for="n in 1" :key="n" color="green" fill-dot right large>
+              <template v-slot:icon>
+                <v-avatar>
+                  <img
+                    src="https://static.wixstatic.com/media/b0cb5d_7a6494c66dfb4bb6b9e2d6c71c214eca~mv2.jpg/v1/fill/w_432,h_434,al_c,lg_1,q_80/Brit_Pro.webp"
+                  />
+                </v-avatar>
+              </template>
+              <template v-slot:opposite>
+                <span class="module-default__faq-question"
+                  >What is the urgency of this problem?</span
+                >
+              </template>
+              <v-card color="green" dark outlined class="elevation-0">
+                <v-card-text
+                  ><div class="module-default__faq-answer">
+                    The pandemic has exacerbated the healthcare system as it goes into overdrive.
+                    People are uncertain and concerned about their current health status.
+                  </div></v-card-text
+                >
+              </v-card>
+            </v-timeline-item>
+
+            <v-timeline-item v-for="n in 1" :key="n" right color="red" fill-dot large>
+              <template v-slot:icon>
+                <v-avatar>
+                  <img
+                    src="https://static.wixstatic.com/media/b0cb5d_7a6494c66dfb4bb6b9e2d6c71c214eca~mv2.jpg/v1/fill/w_432,h_434,al_c,lg_1,q_80/Brit_Pro.webp"
+                  />
+                </v-avatar>
+              </template>
+              <template v-slot:opposite>
+                <span class="module-default__faq-question"
+                  >Who could be possible users of the solution created?</span
+                >
+              </template>
+              <v-card color="red" dark outlined class="elevation-0">
+                <v-card-text class="module-default__faq-answer"
+                  ><div class="module-default__faq-answer">
+                    Frontline workers in healthcare, doctors, and researchers would be the optimal
+                    users of the solution for you bright students to embark on developing.
+                  </div></v-card-text
+                >
+              </v-card>
+            </v-timeline-item>
+
+            <v-timeline-item v-for="n in 1" :key="n" right color="pink" fill-dot large>
+              <template v-slot:icon>
+                <v-avatar>
+                  <img
+                    src="https://static.wixstatic.com/media/b0cb5d_7a6494c66dfb4bb6b9e2d6c71c214eca~mv2.jpg/v1/fill/w_432,h_434,al_c,lg_1,q_80/Brit_Pro.webp"
+                  />
+                </v-avatar>
+              </template>
+              <template v-slot:opposite>
+                <span class="module-default__faq-question">What are the opportunities?</span>
+              </template>
+              <v-card color="pink" dark outlined class="elevation-0">
+                <v-card-text class="module-default__faq-answer"
+                  ><div class="module-default__faq-answer">
+                    We are excited about the idea of bridging the digital divide, enabling
+                    telemedicine practices which would greatly enhance the accessibility low-income
+                    families have with our healthcare system with the factor of transport
+                    eliminated.
+                  </div></v-card-text
+                >
+              </v-card>
+            </v-timeline-item>
+
+            <v-timeline-item v-for="n in 1" :key="n" right color="purple" fill-dot large>
+              <template v-slot:icon>
+                <v-avatar>
+                  <img
+                    src="https://static.wixstatic.com/media/b0cb5d_7a6494c66dfb4bb6b9e2d6c71c214eca~mv2.jpg/v1/fill/w_432,h_434,al_c,lg_1,q_80/Brit_Pro.webp"
+                  />
+                </v-avatar>
+              </template>
+              <template v-slot:opposite>
+                <span class="module-default__faq-question">What are the known challenges?</span>
+              </template>
+              <v-card color="purple" dark outlined class="elevation-0">
+                <v-card-text class="module-default__faq-answer"
+                  ><div class="module-default__faq-answer">
+                    Access to technology seems to be the issue as well as data and privacy
+                    challenges between consumer and provider of healthcare.
+                  </div></v-card-text
+                >
+              </v-card>
+            </v-timeline-item>
+
+            <v-timeline-item v-for="n in 1" :key="n" right color="green" fill-dot large>
+              <template v-slot:icon>
+                <v-avatar>
+                  <img
+                    src="https://static.wixstatic.com/media/b0cb5d_7a6494c66dfb4bb6b9e2d6c71c214eca~mv2.jpg/v1/fill/w_432,h_434,al_c,lg_1,q_80/Brit_Pro.webp"
+                  />
+                </v-avatar>
+              </template>
+              <template v-slot:opposite>
+                <span class="module-default__faq-question"
+                  >Why are you requesting projects from students?</span
+                >
+              </template>
+              <v-card color="green" dark outlined class="elevation-0">
+                <v-card-text class="module-default__faq-answer"
+                  ><div class="module-default__faq-answer">
+                    We have a grant that asks for civic engagement with students to co-generate new
+                    ideas for our proposed solutions. We also are interested in building a pipeline
+                    of opportunities to work in our local industry consortium we lead.
+                  </div></v-card-text
+                >
+              </v-card>
+            </v-timeline-item>
+          </v-timeline>
+        </template>
+      </div>
+      <br />
+      <br />
       <template>
         <v-card outlined elevation="0" max-width="85%" class="mx-auto">
           <v-toolbar elevation="0" dark color="blue">
-            <v-app-bar-nav-icon></v-app-bar-nav-icon>
-            <v-toolbar-title>Project and program details</v-toolbar-title>
+            <v-icon class="mr-4" large>mdi-telegram</v-icon>
+            <v-toolbar-title>Project Specifications</v-toolbar-title>
           </v-toolbar>
           <v-subheader>Outcomes</v-subheader>
           <v-list two-line subheader>
             <v-list-item>
-              <template v-slot:default="{ active }">
-                <v-list-item-action>
-                  <v-checkbox :input-value="active" color="primary"></v-checkbox>
-                </v-list-item-action>
-                <v-list-item-content>
-                  <v-list-item-title>Use human-centered design</v-list-item-title>
-                </v-list-item-content>
-              </template>
-            </v-list-item>
-            <v-list-item>
-              <template v-slot:default="{ active }">
-                <v-list-item-action>
-                  <v-checkbox :input-value="active" color="primary"></v-checkbox>
-                </v-list-item-action>
-                <v-list-item-content>
-                  <v-list-item-title>Solve childhood obesity</v-list-item-title>
-                </v-list-item-content>
-              </template>
-            </v-list-item>
-            <v-list-item>
-              <template v-slot:default="{ active }">
-                <v-list-item-action>
-                  <v-checkbox :input-value="active" color="primary"></v-checkbox>
-                </v-list-item-action>
-                <v-list-item-content>
-                  <v-list-item-title>Use technology</v-list-item-title>
-                </v-list-item-content>
-              </template>
+              <v-chip-group column>
+                <v-chip dark>Build portfolio project</v-chip>
+                <v-chip dark>Qualify for internship to execute on project</v-chip></v-chip-group
+              >
             </v-list-item>
           </v-list>
-          <v-divider></v-divider>
+          <!-- <v-divider></v-divider> -->
           <v-subheader>Deliverables</v-subheader>
           <v-list two-line subheader>
             <v-list-item>
-              <template v-slot:default="{ active }">
-                <v-list-item-action>
-                  <v-checkbox :input-value="active" color="primary"></v-checkbox>
-                </v-list-item-action>
-                <v-list-item-content>
-                  <v-list-item-title>Use human-centered design</v-list-item-title>
-                </v-list-item-content>
-              </template>
-            </v-list-item>
-            <v-list-item>
-              <template v-slot:default="{ active }">
-                <v-list-item-action>
-                  <v-checkbox :input-value="active" color="primary"></v-checkbox>
-                </v-list-item-action>
-                <v-list-item-content>
-                  <v-list-item-title>Solve childhood obesity</v-list-item-title>
-                </v-list-item-content>
-              </template>
-            </v-list-item>
-            <v-list-item>
-              <template v-slot:default="{ active }">
-                <v-list-item-action>
-                  <v-checkbox :input-value="active" color="primary"></v-checkbox>
-                </v-list-item-action>
-                <v-list-item-content>
-                  <v-list-item-title>Use technology</v-list-item-title>
-                </v-list-item-content>
-              </template>
+              <v-chip-group column>
+                <v-chip dark>Business Model Canvas</v-chip>
+                <v-chip dark>One Sentence Pitch</v-chip>
+                <v-chip dark>Elevator Pitch</v-chip>
+                <v-chip dark>Design & Prototype Log</v-chip>
+                <v-chip dark>Prototype Demo</v-chip>
+                <v-chip dark>Presentation Deck</v-chip>
+              </v-chip-group>
             </v-list-item>
           </v-list>
-          <v-divider></v-divider>
+          <!-- <v-divider></v-divider> -->
           <v-subheader>Requirements</v-subheader>
           <v-list two-line subheader>
             <v-list-item>
@@ -121,46 +226,41 @@
               </template>
             </v-list-item>
           </v-list>
-          <v-divider></v-divider>
-          <v-subheader>Get Started Resources</v-subheader>
+          <!-- <v-divider></v-divider> -->
+          <v-subheader>Resources</v-subheader>
           <v-list two-line subheader>
             <v-list-item>
-              <template v-slot:default="{ active }">
-                <v-list-item-action>
-                  <v-checkbox :input-value="active" color="primary"></v-checkbox>
-                </v-list-item-action>
-                <v-list-item-content>
-                  <v-list-item-title>Use human-centered design</v-list-item-title>
-                </v-list-item-content>
-              </template>
-            </v-list-item>
-            <v-list-item>
-              <template v-slot:default="{ active }">
-                <v-list-item-action>
-                  <v-checkbox :input-value="active" color="primary"></v-checkbox>
-                </v-list-item-action>
-                <v-list-item-content>
-                  <v-list-item-title>Solve childhood obesity</v-list-item-title>
-                </v-list-item-content>
-              </template>
-            </v-list-item>
-            <v-list-item>
-              <template v-slot:default="{ active }">
-                <v-list-item-action>
-                  <v-checkbox :input-value="active" color="primary"></v-checkbox>
-                </v-list-item-action>
-                <v-list-item-content>
-                  <v-list-item-title>Use technology</v-list-item-title>
-                </v-list-item-content>
-              </template>
+              <v-btn-group>
+                <v-btn class="ma-1" depressed small outlined>Get Started Folder</v-btn>
+                <v-btn class="ma-1" depressed small outlined>Website</v-btn>
+                <v-btn class="ma-1" depressed small outlined>Instagram</v-btn>
+                <v-btn class="ma-1" depressed small outlined>LinkedIn</v-btn>
+                <v-btn class="ma-1" depressed small outlined>Facebook</v-btn>
+                <!-- <v-btn class="ma-1" depressed small outlined>Articles</v-btn>
+                <v-btn class="ma-1" depressed small outlined>Product Specifications</v-btn>
+                <v-btn class="ma-1" depressed small outlined>Reports</v-btn> -->
+              </v-btn-group>
             </v-list-item>
           </v-list>
+          <v-divider></v-divider>
+          <v-subheader>About</v-subheader>
+          <v-card-text
+            >Kaiser Permanente (/ˈkaɪzər pɜːrməˈnɛnteɪ/; KP) is an American integrated managed care
+            consortium, based in Oakland, California, United States, founded in 1945 by
+            industrialist Henry J. Kaiser and physician Sidney Garfield. Kaiser Permanente is made
+            up of three distinct but interdependent groups of entities: the Kaiser Foundation Health
+            Plan, Inc. (KFHP) and its regional operating subsidiaries; Kaiser Foundation Hospitals;
+            and the regional Permanente Medical Groups. As of 2017, Kaiser Permanente operates in
+            eight states (Hawaii, Washington, Oregon, California, Colorado, Maryland, Virginia,
+            Georgia) and the District of Columbia, and is the largest managed care organization in
+            the United States.</v-card-text
+          >
         </v-card>
       </template>
       <br />
       <br />
-      <div class="module-default__about">
-        <v-btn dense color="blue" small rounded outlined depressed>About</v-btn>
+      <!-- <div class="module-default__about">
+        <v-btn dense color="blue" large rounded outlined depressed>About</v-btn>
         <br />
         <br />
         Kaiser Permanente (/ˈkaɪzər pɜːrməˈnɛnteɪ/; KP) is an American integrated managed care
@@ -173,8 +273,8 @@
         District of Columbia, and is the largest managed care organization in the United States.
       </div>
       <br />
-      <br />
-      <v-divider></v-divider>
+      <br /> -->
+      <!-- <v-divider></v-divider> -->
       <br />
       <br />
       <div class="module-default__scope">
@@ -210,10 +310,10 @@ export default Vue.extend({
 }
 .module-default {
   &__scope {
-    font-size: 20px;
+    font-size: 24px;
     font-weight: 700;
     text-align: center;
-    max-width: 90%;
+    max-width: 95%;
     margin: auto;
   }
   &__youtube {
@@ -236,6 +336,66 @@ export default Vue.extend({
     max-width: 90%;
     margin: auto;
     line-height: 30px;
+  }
+
+  &__faq {
+    font-size: 15px;
+    font-weight: 700;
+    text-align: center;
+    max-width: 90%;
+    margin: auto;
+    line-height: 30px;
+  }
+
+  &__faq-chat {
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+    margin-bottom: 5%;
+  }
+  &__faq-chat-line {
+    margin: 5px;
+  }
+
+  &__faq-avatar {
+    margin: 5px;
+  }
+
+  &__faq-question {
+    // text-align: left;
+    font-family: 'Raleway';
+    font-size: 16px;
+    font-weight: 800;
+    color: #404142;
+  }
+
+  &__faq-answer {
+    text-align: left;
+    font-family: 'Raleway';
+    font-size: 13px;
+    font-weight: 500;
+    letter-spacing: 0px;
+    color: white;
+    font-style: italic;
+  }
+
+  &__faq-answer-dark {
+    text-align: left;
+    font-family: 'Raleway';
+    font-size: 13px;
+    font-weight: 500;
+    letter-spacing: 0px;
+    color: #404142;
+    font-style: italic;
+  }
+
+  &__faq-answer-dark-highlight {
+    text-align: left;
+    font-family: 'Raleway';
+    font-size: 13px;
+    font-weight: 800;
+    letter-spacing: 0px;
+    color: #404142;
   }
 }
 </style>
--- src/Module/components/ModuleSetup.vue
+++ src/Module/components/ModuleSetup.vue
@@ -29,9 +29,112 @@
       ></v-text-field>
       <v-textarea
         outlined
-        label="Enter employer biography"
+        label="About your organization"
         placeholder="In a paragraph or less, describe your organization"
       ></v-textarea>
+      <v-select
+        v-model="outcomesValue"
+        :items="outcomes"
+        chips
+        label="Outcomes"
+        multiple
+        outlined
+      ></v-select>
+      <v-select
+        v-model="deliverablesValue"
+        :items="deliverables"
+        chips
+        label="Deliverables"
+        multiple
+        outlined
+      ></v-select>
+      <template>
+        <v-combobox
+          v-model="chips"
+          outlined
+          flat
+          :items="items"
+          chips
+          multiple
+          label="Project Requirements"
+        >
+          <template v-slot:selection="{ attrs, item, select, selected }">
+            <v-chip
+              v-bind="attrs"
+              :input-value="selected"
+              close
+              @click="select"
+              @click:close="remove(item)"
+            >
+              <strong>{{ item }}</strong
+              >&nbsp;
+              <span></span>
+            </v-chip>
+          </template>
+        </v-combobox>
+      </template>
+
+      <v-text-field
+        outlined
+        label="Instagram"
+        placeholder="https://www.instagram.com/username"
+        prepend-inner-icon="mdi-instagram"
+      ></v-text-field>
+
+      <v-text-field
+        outlined
+        label="LinkedIn"
+        placeholder="https://www.linkedin.com/username"
+        prepend-inner-icon="mdi-linkedin"
+      ></v-text-field>
+
+      <v-text-field
+        outlined
+        label="Facebook"
+        placeholder="https://www.facebook.com/username"
+        prepend-inner-icon="mdi-facebook"
+      ></v-text-field>
+
+      <v-text-field
+        outlined
+        label="Get Started Folder"
+        placeholder="https://drive.google.com/foldername"
+        prepend-inner-icon="mdi-folder-google-drive"
+      ></v-text-field>
+
+      <v-textarea
+        outlined
+        label="What is the urgency of this problem?"
+        placeholder="In a tweet or less, answer the question for student participants."
+      ></v-textarea>
+
+      <v-textarea
+        outlined
+        label="Who could be possible users of the solution created?"
+        placeholder="In a tweet or less, answer the question for student participants."
+      ></v-textarea>
+
+      <v-textarea
+        outlined
+        label="What are the opportunities?"
+        placeholder="In a tweet or less, answer the question for student participants."
+      ></v-textarea>
+
+      <v-textarea
+        outlined
+        label="What are the known challenges?"
+        placeholder="In a tweet or less, answer the question for student participants."
+      ></v-textarea>
+
+      <v-textarea
+        outlined
+        label="Why are you requesting projects from students?"
+        placeholder="In a tweet or less, answer the question for student participants."
+      ></v-textarea>
+
+      <div class="module-default__scope">
+        <v-btn x-large depressed outlined>Save</v-btn>
+      </div>
     </div>
   </v-container>
 </template>
@@ -47,6 +150,35 @@ export default {
     return {
       setupInstructions
     };
+  },
+  data: () => ({
+    outcomes: ['Build portfolio project', 'Qualify for internship to execute on project'],
+    outcomesValue: ['Build portfolio project', 'Qualify for internship to execute on project'],
+    deliverables: [
+      'Business Model Canvas',
+      'One Sentence Pitch',
+      'Elevator Pitch',
+      'Design & Prototype Log',
+      'Prototype Demo',
+      'Presentation Deck'
+    ],
+    deliverablesValue: [
+      'Business Model Canvas',
+      'One Sentence Pitch',
+      'Elevator Pitch',
+      'Design & Prototype Log',
+      'Prototype Demo',
+      'Presentation Deck'
+    ],
+    chips: ['Must use employer product'],
+    items: ['Must use employer product', 'Must use employer service', 'Must use employer process']
+  }),
+
+  methods: {
+    remove(item) {
+      this.chips.splice(this.chips.indexOf(item), 1);
+      this.chips = [...this.chips];
+    }
   }
 };
 </script>

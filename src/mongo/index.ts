4a523ef467eacf36c4730b576e8e39643bd6c15a
juxsta <ericjrreyes2@gmail.com>
2020-10-27 11:25:42 -0700

    rebase

--- src/mongo/index.ts
+++ src/mongo/index.ts
@@ -1,22 +1,13 @@
 import { reactive } from '@vue/composition-api';
+import { MongoClient } from 'mongodb';
 // import { URI, ENVIRONMENT,DATABASE } from config
-let uri = '';
-const dbName = 'DATABASE';
+const uri = 'mongodb://root:password@mongodb:27017';
+const dbName = 'Primary';
 const ENVIRONMENT = 'development';
-// const uri = 'mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority';
-
-if (ENVIRONMENT === 'development') {
-  import { MongoClient } from 'mongodb';
-  // local
-  uri = URI.development;
-} else if (ENVIRONMENT === 'production') {
-  // import { MongoClient } from 'mongodb'; //cloud
-  uri = URI.production;
-}
 
 const client = new MongoClient(uri);
 
-async function update(collectionName: string, query) {
+export async function update(collectionName: string, query) {
   try {
     await client.connect();
     const database = client.db(dbName);
@@ -26,7 +17,7 @@ async function update(collectionName: string, query) {
     await client.close();
   }
 }
-async function read(collectionName: string, query) {
+export async function read(collectionName: string, query) {
   try {
     await client.connect();
     const database = client.db(dbName);
